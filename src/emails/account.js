5484cd3d631e0a88acbf3913c780505a1e9d54aa
Anurag Gupta <aguptaking@gmail.com>
2020-05-23 17:33:35 +0530

    second commit

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,21 +3,31 @@ const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email , name)=>{
-    sgMail.send({
-        to : email,
-        from:"aguptaking@gmail.com",
-        subject: "Welcome to Task Manager App",
-        text : `Thanks for joining ${name}. Let me know how you get to know about this!!`
-    })
+    try{
+        sgMail.send({
+            to : email,
+            from:"aguptaking@gmail.com",
+            subject: "Welcome to Task Manager App",
+            text : `Thanks for joining ${name}. Let me know how you get to know about this!!`
+        })
+    }catch(e){
+
+    }
+    
+    
 }
 
 const sendCancelEmail = (email , name)=>{
-    sgMail.send({
-        to : email,
-        from:"aguptaking@gmail.com",
-        subject: "Sorry to see you go!",
-        text : `GoodBye ${name}. Is their anything we can do for you to keep you onboard!!`
-    })
+    try{
+        sgMail.send({
+            to : email,
+            from:"aguptaking@gmail.com",
+            subject: "Sorry to see you go!",
+            text : `GoodBye ${name}. Is their anything we can do for you to keep you onboard!!`
+        })
+    }catch(e){
+
+    }
 }
 module.exports = {
     sendWelcomeEmail,
